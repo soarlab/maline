524787d3feec91741fef5353de2eecfb14017052
Marko Dimjašević <marko@dimjasevic.net>
2014-05-29 21:17:00 -0600

    Updated emulab scripts so that they reflect usage of the new custom built SDK

--- emulab/set_android_env.sh
+++ emulab/set_android_env.sh
@@ -2,25 +2,15 @@
 
 export MALINE=/mnt/storage/maline
 
-export LOCAL=/mnt/storage
-export SYSDIR=/mnt/storage/android-sdk/system-images/android-19/
-#export BINARY=$LOCAL/android-binary
-export BINARY=$LOCAL/android-binaries/linux-x86/bin
-export SAN=$LOCAL
-RAMDISK=/mnt/ramdisk
-export SDK=$SAN/android-sdk-linux
-# export ANDROID_SDK_HOME=$LOCAL
+export RAMDISK=/mnt/ramdisk
+# ANDROID_SDK_HOME has a default value of ~/.android.                                            
 export ANDROID_SDK_HOME=$RAMDISK
-export ANDROID_SDK_ROOT=$LOCAL/android-sdk
-export ANDROID_HOST_OUT=$LOCAL
+export ANDROID_SDK_ROOT=/mnt/storage/custom-android-sdk
 export ANDROID_TMP=$RAMDISK/.android
 export AVDDIR=$RAMDISK/.android/avd
 
-export LD_LIBRARY_PATH=$LOCAL/android-binaries/linux-x86/lib:$LD_LIBRARY_PATH
+export PATH=$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/tools:$MALINE/bin:$PATH
 
-export PATH=$LOCAL/android-sdk/tools:$LOCAL/android-sdk/platform-tools:$BINARY/bin:$PATH
-# export PATH=/mnt/storage/android-binaries/linux-x86/bin:$BINARY/bin:$PATH
-
-# Add all users to the kvm user group
+# Add all users to the kvm user group                                                            
 sudo usermod -a -G kvm marko
 sudo usermod -a -G kvm simoatze
