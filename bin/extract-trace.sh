e58b49c19326d177be0793f3deeaa6e51a09a242
Marko Dimjašević <marko@cs.utah.edu>
2014-06-09 17:07:14 -0600

    Eliminated the waiting time for an app start, which fixes #2

--- bin/extract-trace.sh
+++ bin/extract-trace.sh
@@ -25,6 +25,21 @@ function __sig_func {
     exit 1
 }
 
+function get_app_pid {
+    # This function will try to fetch app's PID for at most 15s
+    RETRY_COUNT=150
+    __APP_PID=
+    for i in $(seq 1 $RETRY_COUNT); do
+	# Fetch the app PID
+	__APP_PID=`adb -P $ADB_SERVER_PORT shell "ps" | grep -v "USER " | grep $APP_NAME | head -1 | awk -F" " '{print $2}'`
+	if [ ! -z "$APP_PID" ]; then
+    	    break
+	fi
+	sleep 0.1s
+    done
+    eval "$1=$__APP_PID"
+}
+
 # Set traps
 trap __sig_func SIGQUIT
 trap __sig_func SIGKILL
@@ -52,29 +67,26 @@ LOGCATFILE="$APK_FILE_NAME-$APP_NAME-$TIMESTAMP.logcat"
 MONKEY_SEED=42
 
 # Send an event to the app to start it
-echo "Starting the app ..."
+echo "Starting the app... "
 adb -P $ADB_SERVER_PORT shell monkey -p $APP_NAME 1 &>/dev/null
 
-# Give some time to the app to start
-sleep 15s
-
-# Fetch the app PID
-APP_PID=`adb -P $ADB_SERVER_PORT shell "ps" | grep -v "USER " | grep $APP_NAME | head -1 | awk -F" " '{print $2}'`
+# Fetch app's PID
+get_app_pid APP_PID
 
 echo "App's process ID: $APP_PID"
 
 COUNT_PER_ITER=100
 ITERATIONS=10
 
-echo "Testing the app ..."
+echo "Testing the app..."
 echo "There will be up to $ITERATIONS iterations, each sending $COUNT_PER_ITER random events to the app"
 
-echo "Also sending geo-location updates in parallel ..."
+echo "Also sending geo-location updates in parallel..."
 LOCATIONS_FILE="$MALINE/data/locations-list"
 send-locations.sh $LOCATIONS_FILE 0 `cat $LOCATIONS_FILE | wc -l` $CONSOLE_PORT &
 GEO_PID=$!
 
-echo "Spoofing SMS text messages in paralell too ..."
+echo "Spoofing SMS text messages in paralell too..."
 MESSAGES_FILE="$MALINE/data/sms-list"
 send-all-sms.sh $MESSAGES_FILE $CONSOLE_PORT &
 SMS_PID=$!
@@ -84,8 +96,7 @@ for (( i=0; i<$ITERATIONS; i++ )) do
     ACTIVE=`adb -P $ADB_SERVER_PORT shell "ps" | grep $APP_PID`
     # Check if the app is still running. If not, stop testing and stracing
     if [ -z "$ACTIVE" ]; then
-	echo "App not running any more ..."
-	echo "Stopping testing ..."
+	echo "App not running any more. Stopping testing... "
 	break
     fi
 
@@ -104,7 +115,7 @@ for (( i=0; i<$ITERATIONS; i++ )) do
     # Send $COUNT_PER_ITER random events to the app with Monkey, with
     # a delay between consecutive events because the Android emulator
     # is slow, and kill strace once Monkey is done
-    echo "Iteration $i, sending $COUNT_PER_ITER random events to the app ..."
+    echo "Iteration $i, sending $COUNT_PER_ITER random events to the app..."
     timeout 45 adb -P $ADB_SERVER_PORT shell "monkey --throttle 100 -p $APP_NAME -s $MONKEY_SEED $COUNT_PER_ITER && $STRACE_KILL_CMD"
     # Increase the seed for the next round of events
     let MONKEY_SEED=MONKEY_SEED+1
