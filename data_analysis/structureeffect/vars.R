0e217cf2ad5e86d04cdd8a4af08dbae312a8d583
Ivo Ugrina <ivo@iugrina.com>
2014-08-31 18:05:52 +0200

    added structureeffect R code for data analysis

---
+++ data_analysis/structureeffect/vars.R
@@ -0,0 +1,7 @@
+#
+# This file contains basic variables about data location
+# It is intended to be sourced from other files
+#
+
+features.file <- '/common/WORK/iugrina/data/maline_github/data_analysis/data_sparse2.mm'
+output.data.dir <- '/common/WORK/iugrina/data/maline/test'
