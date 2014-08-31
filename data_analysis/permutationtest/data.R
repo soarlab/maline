7542af07b0f496f92f1c5c0b3b615473630cf0fa
Ivo Ugrina <ivo@iugrina.com>
2014-08-31 17:26:37 +0200

    added permutationtest R code

---
+++ data_analysis/permutationtest/data.R
@@ -0,0 +1,25 @@
+#
+# This file will prepare the data for classifiers
+# It is intended to be sourced from other files
+#
+# gives matrix X (covariates) and factor Y (outcome)
+#
+
+# libs
+library(Matrix)
+
+# basic vars
+source('./vars.R')
+
+##############
+# basic data
+##############
+
+D <- readMM(features.file)
+X <- D[,-ncol(D)]
+Y <- factor(D[,ncol(D)], levels=c(0,1), labels=c("goodware", "malware"))
+
+# delete all features that bare no information
+zeros <- (apply(X, 2, max) == 0)
+X <- X[, !zeros]
+
