071d782b68463119b606c56e418ef5c676d2b12a
Marko Dimjašević <marko@cs.utah.edu>
2014-09-19 23:58:57 -0600

    Removed debugging info

--- src/extract-trace.sh
+++ src/extract-trace.sh
@@ -88,8 +88,6 @@ MONKEY_SEED=42
 
 # Start the app and start tracing its system calls
 echo "Starting the app... "
-echo "Process name to look for: $PROC_NAME"
-echo "About to start the app with the following command: am start -n ${APP_NAME}/${ACTIVITY_NAME}"
 adb -P $ADB_SERVER_PORT shell "am start -n ${APP_NAME}/${ACTIVITY_NAME} && set `ps | grep $PROC_NAME` && strace -f -tt -T -p \$2 &>> /sdcard/$LOGFILE" &>/dev/null &
 
 # Fetch app's PID
