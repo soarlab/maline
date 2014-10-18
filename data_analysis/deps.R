2ee63e667fd689e7e05bad5668a013846c0c3c17
Ivo Ugrina <ivo@iugrina.com>
2014-10-18 19:37:03 +0200

    added dependencies deps.R file to data_analysis

---
+++ data_analysis/deps.R
@@ -0,0 +1,12 @@
+# dependencies needed to do the data analysis
+
+install.packages(c("caret",
+                   "doMC",
+                   "e1071",
+                   "foreach",
+                   "glmnet",
+                   "Matrix",
+                   "randomForest",
+                   "rpart",
+                   "rpart.plot",
+                   "RRF"))
