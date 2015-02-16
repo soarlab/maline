53d031f8120ed07d5d9fd2f7e6df468b0aaf1299
Ivo Ugrina <ivo@iugrina.com>
2015-02-16 12:49:39 +0100

    removed job mangling in cv-setup.sh

--- data_analysis/cv-run/cv-setup.sh
+++ data_analysis/cv-run/cv-setup.sh
@@ -10,10 +10,5 @@ for V in $VARS; do
 
 	cp -r ../crossvalidation rundir/${BASE}
 	cp vars/${V} rundir/${BASE}/vars.R
-
-	sed -i "s/CHANGE_THIS/rundir\/${BASE}/" rundir/${BASE}/randomForest.job
-	sed -i "s/CHANGE_THIS/rundir\/${BASE}/" rundir/${BASE}/glmnet-lasso.job
-	sed -i "s/CHANGE_THIS/rundir\/${BASE}/" rundir/${BASE}/glmnet-ridge.job
-	sed -i "s/CHANGE_THIS/rundir\/${BASE}/" rundir/${BASE}/svmLinear.job
 done
 
