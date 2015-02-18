844b0baccb0777d65c2c9e0cbb6dfa900e2759bf
Simone Atzeni <simoatze@raspberry.raspberry.maline.emulab.net>
2015-02-18 11:20:39 -0700

    Adding ad hoc experiments

---
+++ src/create_sets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+create_datasets_cv feature-matrix-graph.sparse.scale ../../folds.csv 1
+create_datasets_cv feature-matrix-graph.sparse.scale ../../folds.csv 2
+create_datasets_cv feature-matrix-graph.sparse.scale ../../folds.csv 3
+create_datasets_cv feature-matrix-graph.sparse.scale ../../folds.csv 4
+create_datasets_cv feature-matrix-graph.sparse.scale ../../folds.csv 5
